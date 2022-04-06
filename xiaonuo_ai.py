3048bb1e38384a276f3e50f2432796890e403ece
Miti_ki <xddhezhangyang@outlook.com>
2022-04-06 14:58:53 +0800

    v0.2

--- xiaonuo_ai.py
+++ xiaonuo_ai.py
@@ -1,7 +1,3 @@
-from enum import Flag
-import imp
-from json.tool import main
-import turtle
 import qq
 import logging
 import random
@@ -9,6 +5,7 @@ from qq.ext import commands
 from xiaonuo_config import appid,token
 from xiaonuo_language import joke
 from xiaonuo_game_language import xng_language
+from xiaonuo_game_data import xng_ud
 
 
 logging.basicConfig(level = logging.DEBUG)
@@ -20,14 +17,22 @@ bot = commands.Bot(command_prefix = "/",owner_id = 114514,intents = intent)
 
 @bot.command(name = "游戏介绍")
 async def game_welcome(ctx:commands.Context):
-    await ctx.channel.send(content = "欢迎游玩小诺的小游戏",msg_id = ctx.message,mention_author=ctx.message.author)
-    await ctx.channel.send(content = "在这一款游戏中，你可以打怪升级，最后打败大boss",msg_id=ctx.message)
-    await ctx.channel.send(content = "本游戏使用纯文字驱动，如果出现Bug，请发送 /bug [出现位置][详细状况]，小诺会记录下来的哦",msg_id=ctx.message)
-    await ctx.channel.send(content = "输入 /初始化游戏 以正式开始游戏",msg_id=ctx.message)
-
+    await ctx.channel.send(content = xng_language["welcome1"],msg_id = ctx.message,mention_author=ctx.message.author)
+    t_tempn = 1
+    for i in range(3):
+        t_tempn = t_tempn+1
+        await ctx.channel.send(content = xng_language["welcome"+str(t_tempn)],msg_id=ctx.message)
+    
 @bot.command(name = "初始化游戏")
 async def game_start(ctx:commands.Context):
-    await ctx.channel.send(content = "成功！" , msg_id = ctx.message,mention_author=ctx.message.author)
+    print(xng_ud)
+    for xng_ud in xng_ud:
+    if xng_ud
+        await ctx.channel.send(content = "成功！" , msg_id = ctx.message,mention_author=ctx.message.author)
+        xng_ud.append(str(ctx.message.author))
+    else:
+        await ctx.channel.send(content = "错误：不能重复初始化" , msg_id = ctx.message,mention_author=ctx.message.author)
+    
 
 @bot.command(name = "讲个笑话",aliases = ["笑话"])
 async def speak_joke(ctx:commands.Context):
