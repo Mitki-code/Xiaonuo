9d6894df22adb30664e4059a387f4b66a86d8673
Miti_ki <xddhezhangyang@outlook.com>
2022-04-06 20:29:48 +0800

    v0.3

--- xiaonuo_ai.py
+++ xiaonuo_ai.py
@@ -9,7 +9,7 @@ import json
 from qq.ext import commands
 
 from xiaonuo_config import appid, token
-from xiaonuo_game_language import xng_language,xng_gps,xng_event
+from xiaonuo_game_language import xng_language,xng_gps,xng_event,xng_temp,xng_map,xng_operate
 from xiaonuo_language import joke
 
 logging.basicConfig(level = logging.DEBUG)
@@ -30,7 +30,9 @@ class xngame(commands.Cog):
         tgps = "x"+str(xng_ud_json["user"+str(ctx.message.author.id)]["x"])+"|"+"y"+str(xng_ud_json["user"+str(ctx.message.author.id)]["y"])
         ginfo_move = ""
         ginfo_event_name = ""
-        ginfo_even_desc = ""
+        ginfo_event_desc = ""
+        ginfo_operate_op_desc = ""
+        
         if xng_gps[tgps][0][1] == 1:
             ginfo_move = ginfo_move+"前/"
         if xng_gps[tgps][0][2] == 1:
@@ -40,21 +42,34 @@ class xngame(commands.Cog):
         if xng_gps[tgps][0][4] == 1:
             ginfo_move = ginfo_move+"右/"
         ginfo_move = ginfo_move.rstrip("/")
-        ginfo_operate = xng_language["operate_desc_0"]+"\n"+xng_language["operate_move"]+"   ["+ginfo_move+"]"+"\n"+xng_language["operate_openbag"]+"   "+xng_language["operate_operate"]
+                
+        if xng_operate[tgps][0] == 0:
+            ginfo_operate_op_desc = xng_language["operate_operate_null"]
+        elif xng_operate[tgps][0] == 1:
+            for i in xng_operate[tgps][1]:
+                ginfo_operate_op_desc = ginfo_operate_op_desc+i+"/"
+        ginfo_operate_op_desca = ginfo_operate_op_desc.rstrip("/")
+        ginfo_operate_op = "["+ginfo_operate_op_desca+"]"
         ginfo_info = xng_language["info_desc_0"]+"\n"+xng_language["info_desc_h"]+str(xng_ud_json["user"+str(ctx.message.author.id)]["max_h"])+"/"+str(xng_ud_json["user"+str(ctx.message.author.id)]["h"])+"   "+xng_language["info_desc_m"]+str(xng_ud_json["user"+str(ctx.message.author.id)]["max_m"])+"/"+str(xng_ud_json["user"+str(ctx.message.author.id)]["m"])+"\n"+xng_language["info_desc_xy"]+"   "+str(xng_gps[tgps][0][0]+"   x"+str(xng_ud_json["user"+str(ctx.message.author.id)]["x"])+"   y"+str(xng_ud_json["user"+str(ctx.message.author.id)]["y"]))
         ginfo_other = xng_language["other_desc_0"]
-        if xng_gps[tgps][5][0] == 1:
-            ginfo_event_name = xng_event[str(xng_gps[tgps][5][1])]["ename"]
-            ginfo_even_desc = xng_event[str(xng_gps[tgps][5][1])]["edesc"]
-        elif xng_gps[tgps][5][0] == 2:
-            print("test")
-        else:
-            print("test")   
-        ginfo_event = ginfo_event_name+"\n"+ginfo_even_desc
+        
+        if xng_temp["map"] == 0:
+            if xng_gps[tgps][5][0] == 1:
+                ginfo_event_name = xng_event[str(xng_gps[tgps][5][1])]["ename"]
+                ginfo_event_desc = xng_event[str(xng_gps[tgps][5][1])]["edesc"]
+            elif xng_gps[tgps][5][0] == 2:
+                print("test")
+            else:
+                print("test")
+        elif xng_temp["map"] == 1:
+            ginfo_event_name = str(xng_gps[tgps][0][0])+"地图"
+            ginfo_event_desc = xng_map[str(xng_gps[tgps][0][0])]
+        ginfo_event = ginfo_event_name+"\n"+ginfo_event_desc
+        ginfo_operate = xng_language["operate_desc_0"]+"\n"+xng_language["operate_move"]+"   ["+ginfo_move+"]"+"\n"+xng_language["operate_operate"]+"   "+ginfo_operate_op+"\n"+xng_language["operate_map"]+"   "+xng_language["operate_openbag"]
         await ctx.channel.send(content = ginfo_event+"\n"+ginfo_info+"\n"+ginfo_operate+"\n"+ginfo_other, msg_id = ctx.message)
 
     async def check_game_guild(ctx:commands.Context):
-        return ctx.message.channel.name == "小游戏" or "小游戏测试"
+        return ctx.message.channel.name == "小游戏" or ctx.message.channel.name == "小游戏测试"
 
     @commands.check(check_game_guild)
     @commands.command(name = "关于")
@@ -150,6 +165,7 @@ class xngame(commands.Cog):
         with open("data/xngame_user_gdata.json", "r") as f:
             xng_ud_json = json.load(f)
         tgps = "x"+str(xng_ud_json["user"+str(ctx.message.author.id)]["x"])+"|"+"y"+str(xng_ud_json["user"+str(ctx.message.author.id)]["y"])
+        xng_temp["map"] = 0
 
         if move == "前":
             if xng_gps[tgps][0][1] == 0:
@@ -197,14 +213,15 @@ class xngame(commands.Cog):
         else:
             await ctx.reply(content= xng_language["operate_move_error_desc_0"])
     
+    @commands.check(check_game_guild)
+    @commands.command(name = "地图")
+    async def game_map(self,ctx:commands.Context):
+        xng_temp["map"] = 1
+
+        await xngame.gio(ctx)
+
     
-
-
-
-
-
-
-
+    
 
 @bot.command(name = "讲个笑话",aliases = ["笑话"])
 async def speak_joke(ctx:commands.Context):
